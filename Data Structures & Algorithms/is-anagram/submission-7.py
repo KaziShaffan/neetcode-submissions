4e15db0d8ae83bc7eee81e0362f8e93394b00b5a
Kazi Shaffan <133924179+KaziShaffan@users.noreply.github.com>
2026-06-03 17:57:43 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False
+        
+        return sorted(s) == sorted(t)
+
+        # if(len(s) != len(t)):
+        #     return False
+
+        # s_dic = {}
+        # t_dic = {}
+        # for i in range(len(s)):
+        #     if s[i] in s_dic:
+        #         s_dic[s[i]] += 1   
+        #     else:
+        #         s_dic[s[i]] = 1
+
+
+        # for i in range(len(t)):
+        #     if t[i] in t_dic:
+        #         t_dic[t[i]] += 1   
+        #     else:
+        #         t_dic[t[i]] = 1
+
+        # print(s_dic)
+        # print(t_dic)
+        # if(s_dic.items() == t_dic.items()):
+        #     return True
+        # return False
+            
